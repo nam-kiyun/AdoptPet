16cc7d47db7f7dbb4bd995f46ce2cb474ebe8b51
htakimsean <dustlrka86@gmail.com>
2025-03-07 20:32:28 +0900

    20250307 08:32 PM

--- src/Board.java
+++ src/Board.java
@@ -1,51 +1,56 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.io.Writer;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Board {
-	private int BoardNum; // 게시판번호
-	private String BoardName; // 게시판제목
-	private HashMap<Integer, Post> postsMap; // 게시글
+public class Board implements Serializable {
+	private int boardNum; // 게시판번호
+	private String boardName; // 게시판제목
+	private String boardPath; // 경로
+	private String base_path = "C:\\AdoptPet\\cat";
 	private int postCounter = 1; // 게시글 번호 증가
-	private String fileName; // 파일 이름
-	private String folderPath = "C:\\AdoptPet\\cat";
+	private HashMap<Integer, Post> postsMap; // 게시글 관리
 
-	public Board(int BoardNum, String BoardName) {
-		this.BoardNum = BoardNum;
-		this.BoardName = BoardName;
-		this.fileName = folderPath + "\\" + BoardName + ".txt"; // 게시판 파일 이름"
+	public Board(String BoardName) {
+		this.boardName = boardName;
+		// this.boardNum = boardNum;
+
+		this.boardPath = base_path; // 게시판 파일 이름"
 		this.postsMap = new HashMap<Integer, Post>();
-		// 저장된 게시글 불러오기
+
 		loadPost();
+
 	}
 
-	// 게시글 생성
-	public Post writePost(String title, String content, String author) {
-		Post post = new Post(postCounter++, title, content, author);
-		postsMap.put(post.getPostNum(), post);
-		savePosts();
-		return post;
+	// 게시글 작성
+	public void writePost(String title, String content, String author) {
+		int postNum = postCounter++;
+		Post post = new Post(postNum, title, author, content);
+
+		// 확인
+		System.out.println("생성된 postNum: " + post.getPostNum());
+
+		postsMap.put(postNum, post);
+		savePosts(post);
 	}
 
 	// 게시글 파일에 저장
-	private void savePosts() {
+	private void savePosts(Post post) {
+		String fileName = boardPath + "\\post_" + post.getPostNum() + ".txt";
 
-		try {
-			PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
 
-			for (Post post : postsMap.values()) {
-					writer.print(post);
-			}
-		} catch (Exception e) {
-			System.out.println("파일을 저장할 수 없습니다." + e.getMessage());
+			writer.write("번호: " + post.getPostNum() + "\n");
+			writer.write("제목: " + post.getTitle() + "\n");
+			writer.write("작성자: " + post.getAuthor() + "\n");
+			writer.write("작성일: " + post.getCreateAt() + "\n");
+			writer.write("내용: " + post.getContent() + "\n");
+
+			System.out.println("게시글 저장완료: " + fileName);
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
@@ -53,17 +58,21 @@ public class Board {
 
 	// 저장된 파일 불러오기
 	private void loadPost() {
-		
+
 	}
 
-	// 게시글 수정 (해당 id랑 author)
+	// 게시글 수정 (해당 postNum/author)
 	public void editPost(int postNum, String newTitle, String newContent) {
 		if (postsMap.containsKey(postNum)) {
 			Post post = postsMap.get(postNum);
-			// post.editPost(newTitle, newContent);
-			savePosts();
+
+			post.setTitle(newTitle);
+			post.setContent(newContent);
+
+			savePosts(post); // 파일 다시 업데이트
+			System.out.println("게시글 수정 완료되었습니다.");
 		} else {
-			System.out.println("해당 게시글이 존재하지 않습니다.");
+			System.out.println("해당 게시글이 없습니다.");
 		}
 	}
 
@@ -74,35 +83,49 @@ public class Board {
 			// 게시글 지우기
 			postsMap.remove(postNum);
 
-			// 게시글 지운거 (파일) 업데이트
-			savePosts();
+			// 기존 파일도 삭제
+			File postFile = new File(boardPath + postNum + ".txt");
+			if (postFile.exists() && postFile.delete()) {
+				System.out.println("게시글" + postNum + "삭제 완료.");
+			} else {
+				System.out.println("해당 번호의 게시글이 없습니다.");
+			}
+
 		}
 	}
 
-	// 특정 게시물 검색
-	public void searchPost() {
-
+	// 모든 게시글 출력 (미정)
+	public void listAllPosts() {
 	}
 
-	// 게시물 정렬
+	// 특정 게시물 검색 (미정)
+	public void searchPost(String word) {
+		boolean find = false;
+
+		for (Post post : postsMap.values()) {
+//			if(post.getTitle().contains(word)) 
+		}
+	}
+
+	// 게시물 정렬 (최신순)
 	public void sortPost() {
 
 	}
 
 	public int getBoardNum() {
-		return BoardNum;
+		return boardNum;
 	}
 
 	public void setBoardNum(int boardNum) {
-		BoardNum = boardNum;
+		this.boardNum = boardNum;
 	}
 
 	public String getBoardName() {
-		return BoardName;
+		return boardName;
 	}
 
 	public void setBoardName(String boardName) {
-		BoardName = boardName;
+		this.boardName = boardName;
 	}
 
 	public HashMap<Integer, Post> getPostsMap() {
@@ -119,14 +142,6 @@ public class Board {
 
 	public void setPostCounter(int postCounter) {
 		this.postCounter = postCounter;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
 	}
 
 }
