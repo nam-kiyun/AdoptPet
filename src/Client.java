f5a14d2541813f2b4d7f04cf55ee02aee4346fe9
byungwook <bw0914@kakao.com>
2025-03-10 15:10:28 +0900

    직렬화, save 기능 구현 및 메뉴추가

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-public class Client extends User {
+public class Client extends User implements Serializable{
 
 	private static String nowUserId;// 현재 로그인된 Client의 id를 저장하는 변수
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
 
 	private String getInput(String message) {// String 값 입력받는 함수
 		System.out.print(message);
 		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			return br.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -101,19 +103,20 @@ public class Client extends User {
 	public void editProfile() {
 		String insertPassWord, changeNickName;
 
-		insertPassWord = getInput("비밀번호 확인: ");
+		insertPassWord = getInput("회원정보 수정을 진행하려면 비밀번호를 입력해주세요: ");
 		if (verifyPassword(insertPassWord)) {
 			// 현재 로그인된 아이디를 키값으로 해당 유저리스트 맵 벨류인 유저 객체의 패스워드를 겟 한 후 입력한 패스워드와 비교
 			while (true) {
-				String choice = getInput("변경할 정보를 선택해주세요(1.비밀번호, 2.닉네임)");
+				String choice = getInput("변경할 정보를 선택해주세요(1.비밀번호, 2.닉네임): ");
 				switch (choice) {
 				case "1":
 					while (true) {
-						String chpass = getInput("변경할 비밀번호를 입력해주세요");
+						String chpass = getInput("변경할 비밀번호를 입력해주세요: ");
 						if (isValidPassword(insertPassWord)) {// password 패턴 확인
 							super.getUserMap().get(nowUserId).setPassword(chpass);
 							// 클라이언트 맵 키값으로 현재 로그인 아이디 값으로 밸류값으로 클라이언트 객체불러와 set로 패스워드 변경
 							System.out.println("비밀번호가 성공적으로 변경되었습니다.");
+							save();
 							break;
 						} else {
 							System.out.println("비밀번호는 영문자, 숫자, 특수문자를 포함해야 하며, 길이는 8~15자여야 하고, 공백은 사용할 수 없습니다.");
@@ -131,7 +134,7 @@ public class Client extends User {
 
 							else {
 								super.getUserMap().get(nowUserId).setNickName(changeNickName); // 패턴과 중복체크 통과시 변경
-								System.out.println("닉네임을 성공적으로 변경했습니다..");
+								System.out.println("닉네임을 성공적으로 변경했습니다.");
 								save();
 								break;
 							}
@@ -144,7 +147,7 @@ public class Client extends User {
 
 					return;
 				default:
-					System.out.print("올바른 값을 입력해주세요(1.비밀번호, 2.닉네임)");
+					System.out.println("올바른 값을 입력해주세요(1.비밀번호, 2.닉네임)");
 					break;
 				}
 			}
@@ -166,6 +169,7 @@ public class Client extends User {
 		} else {
 			System.out.println("비밀번호가 맞지 않습니다.");
 			// 이전 메뉴 호출
+			return;
 		}
 
 	}
@@ -173,7 +177,35 @@ public class Client extends User {
 
 	@Override
 	public void menu() {// 클라이언트 로그인 이후 메뉴
+		while(true) {
+		System.out.println("======================================");
+		System.out.println("1.게시판 목록보기");
+		System.out.println("2.회원정보 수정");
+		System.out.println("3.회원탈퇴");
+		System.out.println("4.종료");
+		System.out.println("======================================");
+		String choice =getInput("원하시는 메뉴를 선택해주세요: ");
 		
+		switch (choice) {
+		case "1":
+			this.setNowUserId();
+			break;
+		case "2": 
+			this.setNowUserId();
+			this.editProfile();
+			break;
+		case "3": 
+			this.setNowUserId();
+			this.deleteAccount();
+			return;
+		case "4": 
+			return;
+
+		default:
+			System.out.println("올바른 메뉴를 선택해주세요");
+			break;
+		}
+		}
 	}
 
 	public Map<String, User> getUsersMap() {
